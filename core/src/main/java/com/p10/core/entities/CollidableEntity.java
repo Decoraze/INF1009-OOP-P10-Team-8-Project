ecb8782f6555b7d1c8e31b545f2bbb7620bb6b76
rumxxnx <2503352@sit.singaporetech.edu.sg>
2026-03-10 02:07:06 +0800

    Made changes to collidableEntity hitbox from private to protected to solve error

--- core/src/main/java/com/p10/core/entities/CollidableEntity.java
+++ core/src/main/java/com/p10/core/entities/CollidableEntity.java
@@ -4,14 +4,13 @@ import com.badlogic.gdx.graphics.glutils.ShapeRenderer;
 import com.badlogic.gdx.math.Rectangle;
 
 public abstract class CollidableEntity extends Entity {
-    private Rectangle hitbox; //
+    protected Rectangle hitbox; //
     private int collisionLayer; //
 
     public CollidableEntity(String id, float x, float y, float w, float h) {
         super(id, x, y);
         this.hitbox = new Rectangle(x, y, w, h);
     }
-
     // Give this method a BODY { } so children don't have to implement it
     public Rectangle getHitbox() {
         return hitbox;
